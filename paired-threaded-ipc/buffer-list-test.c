ba073c48b69ec6b77f17b702417850aa1e8e15e2
AJ ONeal <coolaj86@gmail.com>
2011-01-20 14:18:55 -0700

    bufferlist implementation seems good enough

--- paired-threaded-ipc/buffer-list-test.c
+++ paired-threaded-ipc/buffer-list-test.c
@@ -25,7 +25,8 @@ int main (int argc, char* argv[])
   char* str_buf2 = malloc(strlen(str));
   int bytes_copied = 0;
   memcpy(str_buf2, str, strlen(str) + 1);
-  printf("%s\n%d\n", str_buf2, strlen(str_buf2));
+  printf("%s\n%d\n", str_buf2, (int) strlen(str_buf2));
+
 
   //
   // Buffer
@@ -35,16 +36,16 @@ int main (int argc, char* argv[])
   evn_buffer* buffer0 = evn_buffer_create(128);
   assert(NULL != buffer0);
   assert(NULL != buffer0->data);
-  assert(128 == buffer0->size);
-  assert(0 == buffer0->index);
+  assert(128 == buffer0->capacity);
+  assert(0 == buffer0->used);
 
   // Create a buffer of `size` by `memcpy`ing `data`
   evn_buffer* buffer1 = evn_buffer_create_copy(str, 27);
   printf("abcdefghijklmnopqrstuvwxyz\n==\n%s\n", (char*) buffer1->data);
   assert(NULL != buffer1);
   assert(NULL != buffer1->data);
-  assert(27 == buffer1->size);
-  assert(27 == buffer1->index);
+  assert(27 == buffer1->capacity);
+  assert(27 == buffer1->used);
   assert(0 == memcmp(buffer1->data, str, 27));
   ((char*)buffer1->data)[26] = '\0';
 
@@ -52,16 +53,16 @@ int main (int argc, char* argv[])
   evn_buffer* buffer2 = evn_buffer_create_as(str_buf2, strlen(str) + 1, 27);
   assert(NULL != buffer2);
   assert(NULL != buffer2->data);
-  assert(strlen(str) + 1 == buffer2->size);
-  assert(27 == buffer2->index);
+  assert(strlen(str) + 1 == buffer2->capacity);
+  assert(27 == buffer2->used);
   assert(0 == memcmp(buffer2->data, str, 27));
 
   // Copy `size` bytes of `data` to existing buffer
   bytes_copied = evn_buffer_add(buffer2, "0123456789 ", 11); // discards trailing \0
-  printf("abcdefghijklmnopqrstuvwxyz 0123456789 ...\n==\n%s\n", (char*) buffer2->data);
+  printf("abcdefghijklmnopqrstuvwxyz 0123456789 ...\n==\n%s\n%d\n", (char*) buffer2->data, bytes_copied);
   assert(11 == bytes_copied);
-  assert(strlen(str) + 1 == buffer2->size);
-  assert(38 == buffer2->index);
+  assert(strlen(str) + 1 == buffer2->capacity);
+  assert(38 == buffer2->used);
 
   // TODO evn_buffer_add_realloc
 
@@ -70,20 +71,56 @@ int main (int argc, char* argv[])
   bytes_copied = evn_buffer_add(buffer2, str, 40); // should copy 38 bytes, leaving 2 uncopied
   ((char*)buffer2->data)[75] = '\0';
   printf("abcdefghijklmnopqrstuvwxyz 0123456789 abcdef...\n==\n%s\n", (char*) buffer2->data);
-  assert(strlen(str) + 1 == buffer2->size);
-  assert(buffer2->index == buffer2->size);
-  assert(buffer2->size - 38 == bytes_copied);
+  assert(strlen(str) + 1 == buffer2->capacity);
+  assert(buffer2->used == buffer2->capacity);
+  assert(buffer2->capacity - 38 == bytes_copied);
+
 
   //
   // BufferList
   //
+  printf("BufferList tests...\n");
 
   // Create a "smart" buffer
-  evn_bufferlist* evn_bufferlist_create(int min_block_size, int min_slices);
-/*
+  evn_bufferlist* bufferlist = evn_bufferlist_create(40, 1);
+  assert(40 == bufferlist->block_size);
+  assert(1 == bufferlist->length);
+  assert(0 == bufferlist->used);
+  assert(0 == bufferlist->size);
 
-// Copy data into the buffer 
-int evn_bufferlist_add_data(evn_bufferlist* bufferlist, void* data, int size);
+  // Copy data into the buffer 
+  int result;
+  result = evn_bufferlist_add(bufferlist, &str[60], 15);
+  assert(1 == bufferlist->length);
+  assert(15 == bufferlist->used);
+  assert(40 == bufferlist->size);
+
+  // Copy more data into the buffer than the buffer can hold
+  result = evn_bufferlist_add(bufferlist, &str, 75);
+  assert(2 == bufferlist->length);
+  assert(90 == bufferlist->used);
+  assert(120 == bufferlist->size); // 40 + 80
+
+  // Copy all data into one big buffer
+  evn_buffer* buffer3 = evn_bufferlist_concat(bufferlist);
+  assert(90 == buffer3->used);
+  assert(90 == buffer3->capacity);
+  assert(0 == buffer3->free);
+
+  //
+  // Memory Leak Tests
+  //
+  evn_buffer_destroy(buffer3);
+  evn_buffer_destroy(buffer2);
+  evn_buffer_destroy(buffer1);
+  evn_buffer_destroy(buffer0);
+  evn_bufferlist_destroy(bufferlist);
+/*
+  printf("bs %d\n", bufferlist->block_size);
+  printf("len %d\n", bufferlist->length);
+  printf("used %d\n", bufferlist->used);
+  printf("size %d\n", bufferlist->size);
+
 // Realloc the current buffer and then add a buffer after it
 int evn_bufferlist_add_buffer(evn_bufferlist* bufferlist, evn_buffer* buffer);
 
--- paired-threaded-ipc/buffer-list.c
+++ paired-threaded-ipc/buffer-list.c
@@ -24,18 +24,45 @@ inline evn_buffer* evn_buffer_create(int size)
   evn_buffer* buffer = malloc(sizeof(evn_buffer));
   // TODO check memory
   buffer->data = data;
-  buffer->index = 0;
-  buffer->size = size;
+  buffer->position = data;
+  buffer->used = 0;
+  buffer->capacity = size;
+  buffer->free = size;
 
   return buffer;
+}
+
+// Destory a buffer created with `evn_buffer_create<_xyz_>(int size)`
+void evn_buffer_destroy(evn_buffer* buffer)
+{
+  free(buffer->data);
+  free(buffer);
+}
+
+int evn_buffer_init(evn_buffer* buffer, int size)
+{
+  void* data = malloc(size * sizeof(size_t));
+  if (NULL == data)
+  {
+    return -1;
+  }
+  buffer->data = data;
+  buffer->position = data;
+  buffer->used = 0;
+  buffer->capacity = size;
+  buffer->free = size;
+
+  return 0;
 }
 
 inline evn_buffer* evn_buffer_create_copy(void* data, int size)
 {
   evn_buffer* buffer = evn_buffer_create(size);
   memcpy(buffer->data, data, size);
-  buffer->index = size;
-  buffer->size = size;
+  buffer->position = data + size;
+  buffer->used = size;
+  buffer->capacity = size;
+  buffer->free = 0;
 
   return buffer;
 }
@@ -44,8 +71,10 @@ inline evn_buffer* evn_buffer_create_as(void* data, int size, int index)
 {
   evn_buffer* buffer = malloc(sizeof(evn_buffer));
   buffer->data = data;
-  buffer->size = size;
-  buffer->index = index;
+  buffer->position = data + index;
+  buffer->used = index;
+  buffer->capacity = size;
+  buffer->free = size - index;
 
   return buffer;
 }
@@ -54,7 +83,7 @@ inline evn_buffer* evn_buffer_create_as(void* data, int size, int index)
 // returns the number of bytes added
 int evn_buffer_add(evn_buffer* buffer, void* data, int size)
 {
-  int free_space = buffer->size - buffer->index;
+  int free_space = buffer->capacity - buffer->used;
   int bytes_to_add;
   if (free_space >= size)
   {
@@ -64,8 +93,11 @@ int evn_buffer_add(evn_buffer* buffer, void* data, int size)
   {
     bytes_to_add = free_space;
   }
-  memcpy(&((char *)buffer->data)[buffer->index], data, bytes_to_add);
-  buffer->index += bytes_to_add;
+  memcpy(buffer->position, data, bytes_to_add);
+  buffer->position += bytes_to_add;
+
+  buffer->free -= bytes_to_add;
+  buffer->used += bytes_to_add;
 
   return bytes_to_add;
 }
@@ -73,31 +105,113 @@ int evn_buffer_add(evn_buffer* buffer, void* data, int size)
 // creates bufferlist
 evn_bufferlist* evn_bufferlist_create(int min_block_size, int min_slice_count)
 {
+  if (min_block_size < 1)
+  {
+    min_block_size = 4096;
+  }
+  if (min_slice_count < 1)
+  {
+    min_block_size = 128;
+  }
   evn_bufferlist* bufferlist = (evn_bufferlist*) calloc(1, sizeof(evn_bufferlist));
-  bufferlist->block_size = block_size || 4096;
-  bufferlist->length = slices || 128;
-  bufferlist->index = 0;
+  bufferlist->block_size = min_block_size;
+  bufferlist->length = min_slice_count;
+  bufferlist->index = -1;
   bufferlist->used = 0;
   bufferlist->size = 0;
 
   bufferlist->list = (evn_buffer*) malloc(bufferlist->length * sizeof(evn_buffer));
+  bufferlist->current = bufferlist->list - 1;
   return bufferlist;
 }
 
-/*
+int evn_bufferlist_next_buffer(evn_bufferlist* bufferlist)
+{
+  bufferlist->index += 1;
+  bufferlist->current += 1;
+  if (bufferlist->index == bufferlist->length)
+  {
+    bufferlist->length *= 2;
+    bufferlist->list = (evn_buffer*) realloc(bufferlist->list, bufferlist->length * sizeof(evn_buffer));
+    if (NULL == bufferlist->list)
+    {
+      // TODO printf("Out-of-Memory! Start Panicking!");
+      return -1;
+    }
+    bufferlist->current = bufferlist->list + bufferlist->index;
+  }
+  evn_buffer_init(bufferlist->current, bufferlist->block_size);
+  bufferlist->size += bufferlist->block_size;
+
+  return 0;
+}
 
 // copies data into underlying buffers
 int evn_bufferlist_add(evn_bufferlist* bufferlist, void* data, int size)
 {
-  evn_buffer* buffer = bufferlist->list[bufferlist->index];
+  int total_bytes_copied = 0;
+  int bytes_to_copy = size;
+  int bytes_copied;
 
-  while(bufferlist->block_size < size)
+  while (bufferlist->block_size < size)
   {
     bufferlist->block_size *= 2;
   }
 
-  bufferlist->index
+  if (-1 == bufferlist->index)
+  {
+    evn_bufferlist_next_buffer(bufferlist);
+  }
+
+  while (total_bytes_copied != bytes_to_copy)
+  {
+    bytes_copied = evn_buffer_add(bufferlist->current, data, size);
+    if (bytes_copied != size)
+    {
+      evn_bufferlist_next_buffer(bufferlist);
+    }
+    size -= bytes_copied;
+    data += bytes_copied;
+    total_bytes_copied += bytes_copied;
+  }
+  bufferlist->used += total_bytes_copied;
+
+  // TODO check memory copy
+  return 0;
 }
+
+evn_buffer* evn_bufferlist_concat(evn_bufferlist* bufferlist)
+{
+  int i;
+  void* data = malloc(bufferlist->used);
+  evn_buffer* item = bufferlist->list;
+  evn_buffer* buffer;
+
+  for (i = 0; i < bufferlist->length; i += 1)
+  {
+    memcpy(data, item->data, item->used);
+    item += 1;
+  }
+
+  buffer = evn_buffer_create_as(data, bufferlist->used, bufferlist->used);
+  return buffer;
+}
+
+void evn_bufferlist_destroy(evn_bufferlist* bufferlist)
+{
+  int i;
+  evn_buffer* item = bufferlist->list;
+
+  for (i = 0; i < bufferlist->length; i += 1)
+  {
+    free(item->data);
+    item += 1;
+  }
+
+  free(bufferlist->list);
+  free(bufferlist);
+}
+/*
 
 // adds the buffer, possibly leaving the previous buffer partially used
 int evn_bufferlist_add_buffer(evn_bufferlist* bufferlist, evn_buffer* buffer)
